fe880f164fc8c7d6da634eb115cd0100611fbdd3
ranirajput-codes <02ranirajput9826@gmail.com>
2026-04-28 14:25:49 +0530

    Added the edge case

--- src/binarysearch/CeilingNumber.java
+++ src/binarysearch/CeilingNumber.java
@@ -2,19 +2,17 @@ package binarysearch;
 
 public class CeilingNumber {
     public static void main(String[] args) {
-        int [] arr = {2,4,8,9,10,14,16,19};
+        int [] arr = {2,4,8,9,10,14,16,19,50};
         int target = 15;
         int ans = ceilingOfNumber(arr, target);
         System.out.println(ans);
     }
 //    ceiling of a number. means Find the element, if not found return the smallest bigger element
         static int ceilingOfNumber(int [] arr , int target ){
-
 //        if the target is greater than the greatest number in the array
             if (target > arr[arr.length-1]) {
                 return -1;
             }
-
         int start = 0;
         int end = arr.length-1;
         while(start<=end){
