0711d3d9ec22a200bbc915ca65c1872ec3b30abb
ranirajput-codes <02ranirajput9826@gmail.com>
2026-05-20 10:16:20 +0530

    update solution of LeetCode 34: Find First and Last Position of Element

--- src/binarysearch/FirstAndLastPosition.java
+++ src/binarysearch/FirstAndLastPosition.java
@@ -5,41 +5,37 @@ public class FirstAndLastPosition {
     public static void main(String[] args) {
         int[] nums = {5,7,7,8,8,10};
         int target = 7;
-        int[] ans = searchRange(nums,target);
-        System.out.println(Arrays.toString(ans));
+        int[] answer = searchRange(nums,target);
+        System.out.println(Arrays.toString(answer));
     }
 
     static int[] searchRange(int[] nums, int target) {
-        int[] ans = {-1,-1};
+        int[] answer = {-1, -1};
+        answer[0] = ans(nums, target,true);
+        answer[1] = ans(nums, target, false);
+        return answer;
+
+    }
+
+    static int ans(int[] nums, int target, boolean firstindex){
+        int ans = -1;
         int start = 0;
         int end = nums.length-1;
-        // finding first index
+
         while(start<=end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
-                ans[0] = mid;
-                end = mid - 1;
+                ans = mid;
+                if(firstindex) {
+                    end = mid - 1;  //to find first index
+                }else{
+                    start = mid+1;  //to find second index
+                }
             }
             else if (nums[mid] < target) {
                 start = mid + 1;
             }
             else {
-                end = mid - 1;
-            }
-        }
-
-        //finding second index
-        start = 0;
-        end = nums.length-1;
-        while(start<=end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                ans[1] = mid;
-                start = mid + 1;
-            }
-            else if (nums[mid] < target) {
-                start = mid + 1;
-            }else {
                 end = mid - 1;
             }
         }
