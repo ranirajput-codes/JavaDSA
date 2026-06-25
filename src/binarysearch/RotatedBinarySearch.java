76f3032ae15a96b99c7943323b9fb9abd0f43c84
ranirajput-codes <02ranirajput9826@gmail.com>
2026-06-25 23:45:52 +0530

    Add Leetcode 33: Search in rotated sorted array

--- src/binarysearch/RotatedBinarySearch.java
+++ src/binarysearch/RotatedBinarySearch.java
@@ -2,7 +2,9 @@ package binarysearch;
 
 public class RotatedBinarySearch {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 1;
+        System.out.println(search(nums, target));
     }
     static int search(int[] nums, int target){
         int pivot = findPivot(nums);
