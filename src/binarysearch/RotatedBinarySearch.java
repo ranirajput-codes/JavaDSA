c630adf33577ded60ecfe027976490406b5090d6
ranirajput-codes <02ranirajput9826@gmail.com>
2026-06-26 00:43:03 +0530

    Add Leetcode 33: Search in rotated sorted array

--- src/binarysearch/RotatedBinarySearch.java
+++ src/binarysearch/RotatedBinarySearch.java
@@ -42,6 +42,7 @@ public class RotatedBinarySearch {
         return -1;
     }
 
+    // this will not work with duplicate elements in the array
     static int findPivot(int[] arr){
         int start = 0;
         int end = arr.length;
@@ -60,4 +61,42 @@ public class RotatedBinarySearch {
         }
         return -1;
     }
+    //with duplicates
+    static int findPivotWithDuplicates(int[] arr){
+        int start = 0;
+        int end = arr.length;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            // 4 cases over here
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }if(mid>start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            //if elements at middle, start, end are equal then just skip the duplicates
+            if(arr[mid]==arr[start] && arr[mid]==arr[end]){
+                //skip the duplicates
+
+                //Note: what if these elements at start and end were the pivot?
+                //check if the start is pivot
+                if(arr[start]>arr[start+1]){
+                    return start;
+                }
+                start++;
+                //check whether end is pivot
+                if(arr[end]<arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            }
+            //left side is sorted, so pivot should be right
+            else if(arr[start]<arr[mid] || (arr[start]==arr[mid] && arr[mid] > arr[end])){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
 }
